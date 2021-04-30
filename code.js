69dff1ff1d80aeae39d9e81418b446daacf9248c
OkuhleM <lindokuhlea38@gmail.com>
2021-04-30 12:44:45 +0200

    new information

---
+++ code.js
@@ -0,0 +1,11 @@
+var audio = new Audio('/audio.mp3/bison (1).mp3'); audio.play();
+
+var Audio = new Audio('/audio.mp3/bison (1).mp3');
+     
+     function myAudioFunction(picture) {
+         if(picture == '/audio.mp3/bison (1).mp3') {
+             Audio.play();
+         } else if(picture == 'b') {
+             Audio.play();
+         }
+     }
